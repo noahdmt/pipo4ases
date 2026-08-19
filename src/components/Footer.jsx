94c9dff17db028320dc8bb8470e7ece4dc4b8e3a
noahdmt <noahluch@gmail.com>
2026-08-18 21:26:07 -0300

    fix: footer corregido ya no pis el boton de whatsapp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer({ modoDescanso }) {
   return (
-    <footer className={`max-w-7xl mx-auto px-6 py-4 border-t text-center text-xs font-light relative z-10 w-full ${
+    <footer className={`max-w-7xl mx-auto px-6 pr-20 sm:pr-6 py-4 border-t text-center text-xs font-light relative z-10 w-full ${
       modoDescanso ? 'border-slate-800 text-slate-500' : 'border-white/5 text-slate-500'
     }`}>
       © {new Date().getFullYear()} Pipo 4 Ases Distribuidor Mayorista. Todos los derechos reservados 2026.
